906dad3fb26dbe24c154954d0f451e7ad4378a17
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2025-10-29 17:36:09 -0300

    [Fix] Delete Incorrect association

--- db/migrate/20251029170633_create_reflections.rb
+++ db/migrate/20251029170633_create_reflections.rb
@@ -1,7 +1,6 @@
 class CreateReflections < ActiveRecord::Migration[8.1]
   def change
     create_table :reflections do |t|
-      t.references :user, null: true
       t.references :mood_check_in, null: false, foreign_key: true
       t.text :content
 
